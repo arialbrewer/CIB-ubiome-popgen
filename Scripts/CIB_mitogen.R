a52390fc8198bf259709c629e8680ccd435c64cd
Arial Brewer <arial.brewer@gmail.com>
2025-10-17 15:26:13 -0700

    updated code

--- Scripts/CIB_mitogen.R
+++ Scripts/CIB_mitogen.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(vegan)
 library(strataG)
 library(adegenet)
+library(ape)
 
 #view strataG vignettes
 browseVignettes("strataG")
@@ -15,17 +16,28 @@ setwd("C:/Users/arial/Desktop/Ch.3 Microbiome & popgen/CIB-ubiome-popgen/Data/")
 
 #read in data
 
+#sample metadata
+meta <- read.csv("CIB_sample_metadata.csv")
+
+#O'Corry-Crowe haplotype data
+occ.haps <- strataG::read.fasta("OCorry-Crowe_mthaps.fasta")
+
+#short CIB mtDNA trimmed sequences
+cib.short <- strataG::read.fasta("CIB_sequence_alignment_short.fasta")
+
+#long CIB mtDNA trimmed sequences
+cib.long <- strataG::read.fasta("CIB_sequence_alignment_long.fasta")
+
+#create gtypes object
+data <- df2gtypes(occ.align.haps, ploidy=1, id.col=1)
 
 
-
-seq.df <- dolph.strata[ c("id", "broad", "id")]
-colnames(seq.df)[3] <- "D-loop"
-dl.g <- df2gtypes(seq.df, ploidy = 1, sequences = dolph.seqs)
-dl.g  
 
 #label haplotypes
-dl.haps <- labelHaplotypes(mito.data)
-dl.haps
+seq.df <- dolph.strata[ c("id", "broad")]
+colnames(seq.df)[2] <- "D-loop"
+dl.g <- df2gtypes(seq.df, ploidy = 1, sequences = occ.align.haps)
+dl.g
 
 
 
